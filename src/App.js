0e9f4565683ae788f8b74262cb3b6b4b62ae7253
Anastasiia <nastyadedela@gmail.com>
2024-04-21 20:18:22 +0200

    feat: added loader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import RootLayout from './layouts/RootLayout'
@@ -6,6 +6,8 @@ import HelpLayout from './layouts/HelpLayout'
 import Faq from './pages/help/Faq'
 import Contacts from './pages/help/Contacts'
 import NotFound from './pages/NotFound'
+import CareersLayout from './layouts/CareersLayout'
+import Careers, { careersLoader } from './pages/careers/Careers'
 
 // const router = createBrowserRouter(
 //   createRoutesFromElements(
@@ -40,6 +42,17 @@ const router = createBrowserRouter([
           {
             path: 'contact',
             element: <Contacts />,
+          },
+        ],
+      },
+      {
+        path: 'careers',
+        element: <CareersLayout />,
+        children: [
+          {
+            path: '',
+            element: <Careers />,
+            loader: careersLoader,
           },
         ],
       },
---
+++ src/pages/careers/Careers.js
@@ -0,0 +1,25 @@
+import { Link, useLoaderData } from 'react-router-dom'
+
+function Careers() {
+  const careers = useLoaderData()
+  console.log(careers)
+  return (
+    <div className="careers">
+      {careers.map((career) => (
+        <Link to="/" key={career.id}>
+          <p>{career.title}</p>
+          <p>Based in {career.location}</p>
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+// data loader
+export const careersLoader = async () => {
+  const res = await fetch('http://localhost:4000/careers')
+  const data = res.json()
+  return data
+}
+
+export default Careers
