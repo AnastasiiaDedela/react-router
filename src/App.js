506de1d4c9ddfc241a7072150b072f47604b8df4
Anastasiia <nastyadedela@gmail.com>
2024-04-21 12:53:32 +0200

    feat: inilized project and main pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+
 function App() {
   return (
-    <div className="App">
-      Hello, Ninjas!
-    </div>
-  );
+    <BrowserRouter>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  )
 }
 
 export default App
